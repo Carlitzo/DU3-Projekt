eb903908b92b05234d19617c269eacdb30097deb
nikolinnav <vasic_nikolina@yahoo.com>
2024-04-30 09:35:13 +0200

    header

--- components/header/header.js
+++ components/header/header.js
@@ -7,12 +7,8 @@ function render_header(parent) {
     <p id="name" class="heading">Luxe bites</p>
     <p id="slogan" class="heading">Discover food from around the globe</p>
     <h1 id="current_country></h1> 
-<<<<<<< HEAD
-    <i class="fa-solid fa-user"></i>`;
-=======
     <i class="fa-solid fa-user"></i>
     </div>`
->>>>>>> c19b39ffc192496aa59b067b78dfb9e903872b41
 
     parent.appendChild(header_container);
 }
