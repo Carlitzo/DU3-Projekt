50485abe1e89c1e46393208327272125e425fae3
Carlitzo <adrianscholl95@hotmail.com>
2024-05-21 11:23:57 +0200

    Changed paths

--- components/homepage/homepage.js
+++ components/homepage/homepage.js
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-const wrapper = document.querySelector("#wrapper");
 render_header(wrapper);
 render_nav(wrapper);
 render_footer(wrapper);
-carousel();
-=======
-render_header(wrapper);
-render_nav(wrapper);
-render_footer(wrapper);
->>>>>>> Stashed changes
